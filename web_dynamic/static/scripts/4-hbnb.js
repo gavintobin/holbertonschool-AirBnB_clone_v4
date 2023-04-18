1a7533d790a148ea1a2fea123dc7bbc8f9911ae4
LW068 <105394574+LW068@users.noreply.github.com>
2023-04-18 17:15:07 -0500

    [Feature]: when the 'button' tag is clicked, a new POST request to 'places_search' is made with a list of Amenities checked.

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    const amenity_ids = {};
+    const api = 'http://' + window.location.hostname;
+  
+    $('input[type="checkbox"]').click(function () {
+      if ($(this).is(':checked')) {
+        amenity_ids[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenity_ids[$(this).attr('data-id')];
+      }
+      $('div.amenities h4').text(Object.values(amenity_ids).join(', '));
+    });
+  
+    $.get(api + ':5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    });
+  
+    function fetchPlaces(postData) {
+      $.ajax({
+        type: 'POST',
+        url: api + ':5001/api/v1/places_search/',
+        data: JSON.stringify(postData),
+        contentType: 'application/json',
+        success: function (data) {
+          $('section.places').empty();
+          $('section.places').append('<h1>Places</h1>');
+          for (const place of data) {
+            const placeHTML = `
+              <article>
+                <div class="title">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">
+                    ${place.price_by_night}
+                  </div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">
+                    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${place.max_guest} Guests
+                  </div>
+                  <div class="number_rooms">
+                    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${place.number_rooms} Bedrooms
+                  </div>
+                  <div class="number_bathrooms">
+                    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${place.number_bathrooms} Bathroom
+                  </div>
+                </div>
+                <div class="description">
+                  ${place.description}
+                </div>
+              </article>`;
+            $('section.places').append(placeHTML);
+          }
+        },
+      });
+    }
+  
+    fetchPlaces({});
+  
+    $('button').click(function () {
+      fetchPlaces({ amenities: Object.keys(amenity_ids) });
+    });
+  });
+  
